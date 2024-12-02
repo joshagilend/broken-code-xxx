e32af3a7f9b5317140aebcf07a6385b9257e5147
Josh Stroud <122675906+joshagilend@users.noreply.github.com>
2024-12-02 08:33:32 -0800

    Create error.py

---
+++ error.py
@@ -0,0 +1,21 @@
+imprt os  # Typo in import
+from sys import   # Missing module to import
+import math x  # Incorrect syntax
+def broken_function(a b c): # Missing commas
+  print("This function is broken)  # Missing closing quote
+    return a + b + c  # Indentation issue
+list = [1, 2, 3
+list.append(4)  # Missing closing bracket
+for i in lists:  # Wrong variable name
+if i = 2:  # Single equal sign instead of ==
+  prnt("Found a two!")  # Typo in print
+  continue
+dic = {'key: 'value'}  # Incorrect dictionary syntax
+value = dic['missing_key']  # KeyError waiting to happen
+while True
+    break # SyntaxError: missing colon
+n = int("not_a_number")  # ValueError
+undefined_variable += 1  # NameError
+def wrong_return_func():
+    return return  # SyntaxError
+lambda x: x **)2  # SyntaxError
